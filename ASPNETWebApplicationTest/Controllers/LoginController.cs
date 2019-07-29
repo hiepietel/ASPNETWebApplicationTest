497d00e4ee8eec7e3955b3aedfd0c87970eca2a4
hiepietel <jachumail@yahoo.pl>
2019-07-29 17:08:40 +0200

    added login page

---
+++ ASPNETWebApplicationTest/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using ASPNETWebApplicationTest.Models;
+using ServiceStack.OrmLite;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPNETWebApplicationTest.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Autherize(User user)
+        {
+            var dbFactory = new OrmLiteConnectionFactory(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString, SqlServerDialect.Provider);
+            using (var db = dbFactory.Open())
+            {
+                bool exist = db.Exists<User>(x => x.UserName == user.UserName && x.Password == user.Password);
+                if (exist)
+                 {
+                    Session["userID"] = user.UserName;
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    
+                    return View("Index", user);
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ ASPNETWebApplicationTest/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASPNETWebApplicationTest.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        [System.ComponentModel.DisplayName("User Name")]
+        [Required(ErrorMessage ="This field is required.")]
+        public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "This field is required.")]
+        public string Password {get; set; }
+    }
+}
